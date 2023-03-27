f2e61b55aee91c653330f5e320a6725e56852d79
rainbow5079 <rlaxogh507906@gmail.com>
2023-03-27 10:08:08 +0900

    release

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -10,9 +10,9 @@ const Menu = ({selected_element}) => {
 
     return (
         <div id="menu-container">
-            <a className="all" href="/movies_combine">전체</a>
-            <a className="front" href="/movies_combine/front">프론트 엔드</a>
-            <a className="back" href="/movies_combine/back">백 엔드</a>
+            <a className="all" href="">전체</a>
+            <a className="front" href="/front">프론트 엔드</a>
+            <a className="back" href="/back">백 엔드</a>
         </div>
     )
 }
